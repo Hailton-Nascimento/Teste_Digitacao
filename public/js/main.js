085a3c8eba602f19c6f53b09b120baad8f84edcd
Hailton Nascimento <67934954+Hailton-Nascimento@users.noreply.github.com>
2021-05-06 10:53:50 -0300

    Criando eventos de escuta de entrada de dados

--- public/js/main.js
+++ public/js/main.js
@@ -4,5 +4,34 @@
 var frase = $(".frase").text();
 var numPalavras = frase.split(" ").length;
 
-
 $('#numPalavras').text(numPalavras);
+
+
+// Adicionando eventos 
+
+var campo = $(".campo-digitacao");
+campo.on("input", () => {
+    var entrada = campo.val();
+
+    var qtdPalavras = entrada.split(/\S+/).length - 1;
+    $('#contador-palavras').text(qtdPalavras);
+
+    var qtdCaracteres = entrada.length;
+    $('#contador-caracteres').text(qtdCaracteres);
+});
+
+// add contador de tempo
+
+var tempoRestante = $("#tempo-digitacao").text();
+campo.one("focus", () => {
+    var cronometroID = setInterval(() => {
+        tempoRestante--;
+        
+        $("#tempo-digitacao").text(tempoRestante);
+        if (tempoRestante < 1) {
+            campo.attr("disabled",true);
+            clearInterval(cronometroID);
+        }
+    }, 1000);
+        
+});
