ca65cb4c9ea2e6e6b1c82b1c4b0506f3db1d64f5
Hailton Nascimento <67934954+Hailton-Nascimento@users.noreply.github.com>
2021-05-06 13:51:27 -0300

    Impementando função reinicia Jogo

--- public/js/main.js
+++ public/js/main.js
@@ -1,45 +1,92 @@
+const tempoInicial = $("#tempo-digitacao").text()
+var campo = $(".campo-digitacao");
+
+
+// atalho para Funciton para
+// $(document).ready(Funciton => {
+// });
+
+// $(Funciton(){
+//     executa tudo que esta aqui apos carregar a pagina.
+//     }
+// )
+
+
+
+
+
+$(document).ready(() => {
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    evitaFraudes();
+    $("#botao-reiniciar").on("click", reiniciaJogo);
+});
+
 
 //Contando numero de plavras nas frase e incluindo no HTML.
 
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+    $('#numPalavras').text(numPalavras);
 
-$('#numPalavras').text(numPalavras);
+}
 
 
 // Adicionando eventos 
 
-var campo = $(".campo-digitacao");
-campo.on("input", () => {
-    var entrada = campo.val();
+function inicializaContadores() {
 
-    var qtdPalavras = entrada.split(/\S+/).length - 1;
-    $('#contador-palavras').text(qtdPalavras);
+    campo.on("input", () => {
+        var entrada = campo.val();
 
-    var qtdCaracteres = entrada.length;
-    $('#contador-caracteres').text(qtdCaracteres);
-});
+        var qtdPalavras = entrada.split(/\S+/).length - 1;
+        $('#contador-palavras').text(qtdPalavras);
+
+        var qtdCaracteres = entrada.length;
+        $('#contador-caracteres').text(qtdCaracteres);
+    });
+}
 
 // add contador de tempo
 
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus", () => {
-    const cronometroID = setInterval(() => {
-        tempoRestante--;
-        
-        $("#tempo-digitacao").text(tempoRestante);
-        if (tempoRestante < 1) {
-            campo.attr("disabled",true);
-            clearInterval(cronometroID);
-        }
-    }, 1000)
-        
-});
+function inicializaCronometro() {
+    var tempoRestante = tempoInicial;
+    $("#tempo-digitacao").text(tempoRestante);
+    campo.one("focus", () => {
+        const cronometroID = setInterval(() => {
+            tempoRestante--;
 
-campo.bind('cut copy paste', function(event) {
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1) {
+                campo.attr("disabled", true);
+                clearInterval(cronometroID);
+            }
+        }, 1000)
+
+    });
+
+}
+
+function evitaFraudes() {
+    campo.bind('cut copy paste', function (event) {
         event.preventDefault();
         console.log("não pode colar no campo")
         alert(`Não pode trapacear!!
-        Deus tá vendo!` );
-       
-    }); 
+            Deus tá vendo!
+            O jogo será reiniciado.` );
+        reiniciaJogo();
+
+    });
+}
+
+function reiniciaJogo() {
+
+    campo.attr("disabled", false);
+    campo.val('');
+    $('#contador-palavras').text(0);
+    $('#contador-caracteres').text(0);
+    inicializaCronometro();
+}
+
